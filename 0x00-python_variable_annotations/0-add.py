467236579145d43c82dbc45518d0240fbc502ccd
KiPTM <mathewtuwei@gmail.com>
2024-09-11 12:37:56 +0000

    Py var

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Annotations in python"""
+
+
+def add(a: float, b: float) -> float:
+    """function add which takes a float a and a
+        float b as arguments and returns
+        their sum as a float.
+    """
+
+    return float(a + b)
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Import modules"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+        Return the floor of the given float.
+
+        args:
+        n (float): The float number to find the floor of
+    """
+
+    return math.floor(n)
